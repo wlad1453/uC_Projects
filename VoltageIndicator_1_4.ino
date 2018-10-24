7737ef464ef35217941359242304f55b1aad1051
Vladimir Filippov <vfabian@mail.ru>
2018-10-24 13:56:59 +0300

    Update VoltageIndicator_1_4.ino

--- VoltageIndicator_1_4.ino
+++ VoltageIndicator_1_4.ino
@@ -1,11 +1,28 @@
 /*
-  
+This regulator is developed with the goal to optimize the working of a heating system pump.
+It has three voltage sensors with galvanic deviding trafos which are connected to the servo valves of the heating lines. 
+In case there is 220V on the sensor input the /uC recieves an AC signal from 0 to 5 V of the amplitude.
+The signal from three sensors are being evaluating and a control command will be issued to the relay in folloving cases:
+- at lease one of the sensors has 220V on its input, the relay should be set to the ON-status
+- no one sensor has an apporpriate voltage level, the relay should switch off the heating pump.
+In course of the switching on or off procedure relay should change its status after a sertain delay, 
+wich in turn can be possible changed in framework of the setup sequence. Usually this delay amount to 0..5 minutes.
+The whole regulater should be controlled via one button and can be put in one of the three possible states: ON, OFF and Automatic.
+This button reacts for the single pressing less then 1 sec. and switches betwenn the mentioned states.
+If the button is pressed for 5 sec. the setup routine is entered. WIthin this routine the delay time can be set for 
+the switch-on and -off procedure separately.
 */
 
 #define sensorPin_1 1
 #define sensorPin_2 2
 #define sensorPin_3 3
 #define relayPin 12
+#define led1 7
+#define led2 8
+#define led3 9
+#define buttonPin 14
+#define controlLED 16
+#define setUpLED 18
 
 
 boolean switching(false);  // Any switching process is going on
@@ -14,10 +31,10 @@ boolean relayOn(false);    // Status of the relay
 unsigned long blinkTimer(0);
 unsigned long onOffTimer(0);
 
-int switchingOnDelay(5  000), switchingOffDelay(15000);
+int switchingOnDelay(5000), switchingOffDelay(15000);
 int blinkPeriod(1000);
 
-int sensorSignal[3][10]{};        // analog signal from 3 voltage sensors X 10 samples
+int sensorSignal[3][10]{};        // analog signal from 3 voltage sensors x 10 samples
 int voltage[3]{};                 // average voltage signal from 3 sensors
 int average = 513;                // threshold on/off
 int voltageSum(0), vMax(10), vMin(10000);
@@ -25,7 +42,7 @@ int voltageSum(0), vMax(10), vMin(10000);
 
 // *****------ S-E-T-U-P---------****************************************
 void setup() {
-  Serial.begin(9600);           // Used to type in characters and for setUp
+  Serial.begin(9600);           // Serial link to PC created
   
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(relayPin, OUTPUT);
